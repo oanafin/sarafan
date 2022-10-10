59a8850c9d4e1760af11c5f9b4e2503aed0ae200
olzhas <olzhas.anafin@gmail.com>
2022-10-10 16:04:39 +0600

    Add vue-router

--- src/main/resources/static/js/pages/App.vue
+++ src/main/resources/static/js/pages/App.vue
@@ -2,50 +2,56 @@
     <v-app>
         <v-toolbar app>
             <v-toolbar-title>Sarafan</v-toolbar-title>
+            <v-btn  flat 
+                    v-if="profile"
+                    :disabled="$route.path === '/'"
+                    @click="showMessages">
+                Messages
+            </v-btn>
             <v-spacer></v-spacer>
-            <span v-if="profile">{{profile.name}}</span>
+            <v-btn  flat
+                    v-if="profile"
+                    :disabled="$route.path === '/profile'"
+                    @click="showProfile">
+                {{profile.name}}
+            </v-btn>
             <v-btn v-if="profile" icon href="/logout">
                 <v-icon>exit_to_app</v-icon>
             </v-btn>
-        </v-toolbar app>
+        </v-toolbar>
         <v-content>
-            <v-container v-if="!profile">Необходимо авторизоваться через <a href="/login">Google</a></v-container>
-            <v-container v-if="profile">
-                <messages-list :messages="messages" />
-            </v-container>
+            <router-view></router-view>
         </v-content>
     </v-app>
 </template>
 
 <script>
-    import MessagesList from 'components/messages/MessageList.vue'
+    import { mapState, mapMutations } from 'vuex'
     import { addHandler } from 'util/ws'
     export default {
-        components: {
-            MessagesList
-        },
-        data() {
-            return {
-                messages: frontendData.messages,
-                profile: frontendData.profile
+        computed: mapState(['profile']),
+        methods: {
+            ...mapMutations(['addMessageMutation','updateMessageMutation','removeMessageMutation']),
+            showMessages() {
+                this.$router.push("/")
+            },
+            showProfile() {
+                this.$router.push("/profile")
             }
         },
         created() {
             addHandler(data => {
                 if (data.objectType === 'MESSAGE') {
-                    const index = this.messages.findIndex(item => item.id === data.body.id)
                     console.log(data)
                     switch (data.eventType) {
                         case 'CREATE':
+                            this.addMessageMutation(data.body)
+                            break
                         case 'UPDATE':
-                            if (index > -1) {
-                                this.messages.splice(index, 1, data.body)
-                            } else {
-                                this.messages.push(data.body)
-                            }
+                            this.updateMessageMutation(data.body)
                             break
                         case 'REMOVE':
-                            this.messages.splice(index, 1)
+                            this.removeMessageMutation(data.body)
                             break
                         default:
                             console.error(`Looks like the event type if unknown ${data.eventType}`)
@@ -54,6 +60,11 @@
                     console.error(`Looks like the object ${data.objectType} type if unknown `)
                 }
             })
+        },
+        beforeMount() {
+            if (!this.profile) {
+                this.$router.replace('/auth')
+            }
         }
     }
 </script>
