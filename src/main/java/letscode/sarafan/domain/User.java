ba29b8066c408bf46e916b704127175a489e91af
olzhas <olzhas.anafin@gmail.com>
2022-10-13 22:55:12 +0600

    Add subscriptions

--- src/main/java/letscode/sarafan/domain/User.java
+++ src/main/java/letscode/sarafan/domain/User.java
@@ -1,85 +1,67 @@
 package letscode.sarafan.domain;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonView;
+import com.fasterxml.jackson.annotation.*;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name="usr")
 @Data
+@EqualsAndHashCode(of = { "id" })
 public class User implements Serializable {
+
     @Id
     @JsonView({Views.IdName.class})
     private String id;
+
     @JsonView({Views.IdName.class})
     private String name;
+
     @JsonView({Views.IdName.class})
     private String userpic;
+
     private String email;
+
+    @JsonView({Views.FullProfile.class})
     private String gender;
+
+    @JsonView({Views.FullProfile.class})
     private String locale;
+
+    @JsonView({Views.FullProfile.class})
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime lastVisit;
 
-    public String getId() {
-        return id;
-    }
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_subscriptions",
+            joinColumns = @JoinColumn(name = "subscriber_id"),
+            inverseJoinColumns = @JoinColumn(name = "channel_id"))
+    @JsonIdentityReference
+    @JsonIdentityInfo(
+            property = "id",
+            generator = ObjectIdGenerators.PropertyGenerator.class
+    )
+    @JsonView({Views.FullProfile.class})
+    private Set<User> subscriptions = new HashSet<>();
 
-    public void setId(String id) {
-        this.id = id;
-    }
+    @ManyToMany
+    @JoinTable(
+            name = "user_subscriptions",
+            joinColumns = @JoinColumn(name = "channel_id"),
+            inverseJoinColumns = @JoinColumn(name = "subscriber_id"))
+    @JsonView({Views.FullProfile.class})
+    @JsonIdentityReference
+    @JsonIdentityInfo(
+            property = "id",
+            generator = ObjectIdGenerators.PropertyGenerator.class
+    )
+    private Set<User> subscribers = new HashSet<>();
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getUserpic() {
-        return userpic;
-    }
-
-    public void setUserpic(String userpic) {
-        this.userpic = userpic;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public String getLocale() {
-        return locale;
-    }
-
-    public void setLocale(String locale) {
-        this.locale = locale;
-    }
-
-    public LocalDateTime getLastVisit() {
-        return lastVisit;
-    }
-
-    public void setLastVisit(LocalDateTime lastVisit) {
-        this.lastVisit = lastVisit;
-    }
 }
--- src/main/resources/static/js/components/comment/CommentItem.vue
+++ src/main/resources/static/js/components/comment/CommentItem.vue
@@ -1,21 +1,21 @@
 <template>
     <v-list-tile>
-        <v-list-tile-avatar>
-            <img :src="comment.author.userpic">
-        </v-list-tile-avatar>
+        <user-link
+            :user="comment.author"
+        ></user-link>
         <v-list-tile-content>
             <v-list-tile-title> {{comment.text}} </v-list-tile-title>
-            <v-list-tile-sub-title> {{comment.author.name}} </v-list-tile-sub-title>
         </v-list-tile-content>
     </v-list-tile>
 </template>
 
 <script>
+import UserLink from 'components/UserLink.vue';
     export default {
-        name: 'CommentItem',
-        props: ['comment']
-        
-    }
+    name: "CommentItem",
+    props: ["comment"],
+    components: { UserLink }
+}
 </script>
 
 <style scoped>
